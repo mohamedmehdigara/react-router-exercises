4389d2746d9026a1c0fef99abc2b7684d2261c78
mohamedmehdigara <garamehdi57@yahoo.com>
2023-08-07 10:15:05 +0100

    added files

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
+import Students from "./Students";
 
-const Home = () => (
-    <h1>Home</h1>
-);
-
-const About = () => (
-    <h1>About</h1>
-);
-
-const Contact = () => (
-    <h1>Contact Us</h1>
+const Navigation = () => (
+    <ul>
+        <li>
+            <Link to="/">Home</Link>
+        </li>
+        <li>
+            <Link to="/about">About</Link>
+        </li>
+        <li>
+            <Link to="/contact">Contact Us</Link>
+        </li>
+    </ul>
 );
 
 const App = () => (
     <div>
         <h1>Hello, world!</h1>
-        <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact Us</li>
-        </ul>
+        <Navigation />
 
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} />
     </div>
 );
-
 export default App;
